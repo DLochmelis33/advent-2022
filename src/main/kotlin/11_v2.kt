5f9fcdb1c1f1edc2a986114d84106f31574a7b47
dlochmelis33 <dlochmelis@gmail.com>
2022-12-14 15:21:06 +0100

    Solve task 11b

--- src/main/kotlin/11_v2.kt
+++ src/main/kotlin/11_v2.kt
@@ -1,22 +1,26 @@
-typealias MonkeyItem = Int
+// to see solution of part 1, see previous commit 7f3f9e00ccded640e9f82267798274eb0a126145
+
+typealias MonkeyItem = Long
 typealias Midx = Int
 
+var gcd = 1L
+
 class Monkey(
-    items: List<Int>,
+    items: List<MonkeyItem>,
     private val itemOp: (MonkeyItem) -> MonkeyItem,
-    private val testModulo: Int,
+    val testModulo: Long,
     private val onTrue: Midx,
     private val onFalse: Midx,
-    val monkeys: List<Monkey>
+    private val monkeys: List<Monkey>
 ) {
     private val items = items.toMutableList()
-    var cnt = 0
+    var cnt = 0L
         private set
 
     fun act() {
         for (item in items) {
-            val newItem = itemOp(item) / 3
-            (if (newItem % testModulo == 0) monkeys[onTrue] else monkeys[onFalse]).items.add(newItem)
+            val newItem = itemOp(item) % gcd
+            (if (newItem % testModulo == 0L) monkeys[onTrue] else monkeys[onFalse]).items.add(newItem)
             cnt++
         }
         items.clear()
@@ -27,17 +31,17 @@ val monkeys = mutableListOf<Monkey>()
 
 fun readMonkey() {
     readln() // "monkey i"
-    val startingItems = readln().removePrefix("  Starting items: ").split(", ").map { it.toInt() }
-    val itemOp: (Int) -> Int = readln().removePrefix("  Operation: new = ").let { expr ->
+    val startingItems = readln().removePrefix("  Starting items: ").split(", ").map { it.toLong() }
+    val itemOp: (MonkeyItem) -> MonkeyItem = readln().removePrefix("  Operation: new = ").let { expr ->
         if (expr == "old * old") {
             { it * it }
         } else "old \\* (\\d+)".toRegex().matchEntire(expr)?.let { mr ->
-            { it * mr.groupValues[1].toInt() }
+            { it * mr.groupValues[1].toLong() }
         } ?: "old \\+ (\\d+)".toRegex().matchEntire(expr)?.let { mr ->
-            { it + mr.groupValues[1].toInt() }
+            { it + mr.groupValues[1].toLong() }
         } ?: error("unknown operation '$expr'")
     }
-    val testModulo = readln().removePrefix("  Test: divisible by ").toInt()
+    val testModulo = readln().removePrefix("  Test: divisible by ").toLong()
     val onTrue = readln().removePrefix("    If true: throw to monkey ").toInt()
     val onFalse = readln().removePrefix("    If false: throw to monkey ").toInt()
 
@@ -50,9 +54,10 @@ fun main() {
         readMonkey()
         readln()
     }
-    repeat(20) {
+    gcd = monkeys.map { it.testModulo }.reduce(Long::times)
+    repeat(10000) {
         for (m in monkeys) m.act()
     }
     println(monkeys.map { it.cnt })
-    println(monkeys.sortedByDescending { it.cnt }.map { it.cnt }.take(2).reduce(Int::times))
+    println(monkeys.sortedByDescending { it.cnt }.map { it.cnt }.take(2).reduce(Long::times))
 }
