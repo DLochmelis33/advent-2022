641a81a735574a48f4e98fbd64bd1c6a3951c885
dlochmelis33 <dlochmelis@gmail.com>
2022-12-27 01:52:35 +0100

    Solve task 20b

--- src/main/kotlin/20.kt
+++ src/main/kotlin/20.kt
@@ -1,27 +1,31 @@
 import java.io.File
 
-private data class Item(val value: Int, val next: Item?) {
+private data class Item(val value: Long, val next: Item?) {
     override fun toString() = value.toString()
 }
+
+private const val decryptKey = 811589153L
 
 fun main() {
     val input = File("in.txt").readLines().map { it.toInt() }
     val items = input.foldRight(mutableListOf<Item>()) { v, acc ->
-        acc.apply { add(0, Item(v, acc.firstOrNull())) }
+        acc.apply { add(0, Item(v * decryptKey, acc.firstOrNull())) }
     }
     val initItems = items.toList()
 
-    for (item in initItems) {
-        val i = items.indexOf(item)
-        val j = item.value.let {
-            val wrap = items.size - 1
-            val j = i + it
-            (if (j > 0) j else j - (j.floorDiv(wrap)) * wrap) % wrap
+    repeat(10) {
+        for (item in initItems) {
+            val i = items.indexOf(item)
+            val j = item.value.let {
+                val wrap = items.size - 1
+                val j = i + it
+                (if (j > 0) j else j - (j.floorDiv(wrap)) * wrap) % wrap
+            }.toInt()
+            items.add(j, items.removeAt(i))
         }
-        items.add(j, items.removeAt(i))
     }
 
-    val zi = items.indexOfFirst { it.value == 0 }
+    val zi = items.indexOfFirst { it.value == 0L }
     fun getIth(i: Int) = items[(zi + i) % items.size]
 
     val result = listOf(1000, 2000, 3000).sumOf { getIth(it).value }
